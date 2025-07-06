12928cf093c37bd5a1a47877b19284ceedb28d24
Emmanuel Aroso <57766083+emarc99@users.noreply.github.com>
2025-07-06 23:55:36 +0100

    init

--- src/flare_ai_kit/ecosystem/__init__.py
+++ src/flare_ai_kit/ecosystem/__init__.py
@@ -4,4 +4,4 @@ from .explorer import BlockExplorer
 from .flare import Flare
 from .protocols import FtsoV2
 
-__all__ = ["BlockExplorer", "Flare", "FtsoV2"]
+__all__ = ["BlockExplorer", "Flare", "FtsoV2", "FDC"]
