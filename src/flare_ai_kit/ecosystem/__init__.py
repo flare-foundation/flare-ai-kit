a4d505d13eb015d48daf6d53690a94bd4c36471e
Emmanuel Aroso <57766083+emarc99@users.noreply.github.com>
2025-08-26 21:49:07 +0100

    chore: init

--- src/flare_ai_kit/ecosystem/__init__.py
+++ src/flare_ai_kit/ecosystem/__init__.py
@@ -3,5 +3,6 @@
 from .explorer import BlockExplorer
 from .flare import Flare
 from .protocols import FtsoV2
+from .protocols import Fdc
 
-__all__ = ["BlockExplorer", "Flare", "FtsoV2", "FDC"]
+__all__ = ["BlockExplorer", "Flare", "FtsoV2", "Fdc"]
--- src/flare_ai_kit/ecosystem/protocols/__init__.py
+++ src/flare_ai_kit/ecosystem/protocols/__init__.py
@@ -1,3 +1,4 @@
 from .ftsov2 import FtsoV2
+from .fdc import Fdc
 
-__all__ = ["FtsoV2"]
+__all__ = ["FtsoV2", "Fdc"]
