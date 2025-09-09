aa2e14ea92d394d8eda523950771fd72ca97aee7
Simon Jonsson <simonjonsson87@gmail.com>
2025-09-09 21:33:24 +0100

    fix(ra-tls-client): ruff check fix

--- src/flare_ai_kit/ecosystem/__init__.py
+++ src/flare_ai_kit/ecosystem/__init__.py
@@ -3,7 +3,6 @@
 from .explorer import BlockExplorer
 from .flare import Flare
 from .protocols import DataAvailabilityLayer, FAssets, FtsoV2
-from .protocols import FAssets, FtsoV2
 from .settings import (
     ChainIdConfig,
     ContractAddresses,
